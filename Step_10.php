8be9b3254edc39b1d837ab62c2da49f6ff276209
Nabigha Mogharbel <nabighamogharbel@gmail.com>
2023-02-19 02:15:26 +0200

    step 10, for each ex 2

---
+++ Step_10.php
@@ -0,0 +1,20 @@
+<?php
+
+$transactions = array(
+    array(
+        "id" => 1,
+        "name"=> "Gaby",
+        "email"=> "gaby@codi.tech"
+    ),
+    array(
+       "id" => 3,
+       "name"=> "Omar",
+       "email"=> "omar@codi.tech"
+    )
+);
+foreach($transactions as $array){
+foreach($array as $key => $value){
+    echo "$key:$value ";
+}
+echo PHP_EOL;}
+?>
